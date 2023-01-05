9d71cbf3bcd4a3e4124db8d71d9719beb038cd72
Dan Havercroft <Dan Havercroft>
2023-01-05 08:39:55 +0000

    Initial Commit

---
+++ FaceNorth Watch App/FaceNorthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceNorthApp.swift
+//  FaceNorth Watch App
+//
+//  Created by Daniel Havercroft on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceNorth_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
